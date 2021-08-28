0f3271a4f179ee00aaea2c7d322e918392eb2028
SKM <skm@Stevens-MacBook-Air.local>
2021-08-28 16:28:37 -0500

    map added

--- app.py
+++ app.py
@@ -15,54 +15,74 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 
 
 # Read in data from public sources
 pop = int(330593072)
-data = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
-data = pd.read_csv(data)
-data['date'] = pd.to_datetime(data['date'])
+states = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+counties_str = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+fips = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+
+states = pd.read_csv(states)
+counties_df = pd.read_csv(counties_str)
+states['date'] = pd.to_datetime(states['date'])
+counties_df['date'] = pd.to_datetime(counties_df['date'])
+fips['date'] = pd.to_datetime(fips['date'])
+
+counties_df = pd.merge(counties_df,fips, on='fips',how='left')
 
 # create columns based on provided data relative to individual weekly numbers and US population
-data['new_cases'] = data['cases'].diff(periods=1)
-data['new_deaths'] = data['deaths'].diff(periods=1)
-data['cases_per_pop'] = data['cases'].apply(lambda x: (x/pop)*100)
-data['deaths_per_pop'] = data['deaths'].apply(lambda x: (x/pop)*100)
-data['mortality_rate'] = (data['deaths']/data['cases'])*100
-data['case_rate'] = ((data.new_cases - data.new_cases.shift(1)) / data.new_cases.shift(1))*100
+states['new_cases'] = states['cases'].diff(periods=1)
+states['new_deaths'] = states['deaths'].diff(periods=1)
+states['cases_per_pop'] = states['cases'].apply(lambda x: (x/pop)*100)
+states['deaths_per_pop'] = states['deaths'].apply(lambda x: (x/pop)*100)
+states['mortality_rate'] = (states['deaths']/states['cases'])*100
+states['case_rate'] = ((states.new_cases - states.new_cases.shift(1)) / states.new_cases.shift(1))*100
 
 pd.set_option('use_inf_as_na', True)
-data.fillna(0, inplace=True)
+states.fillna(0, inplace=True)
 
 #Set 7, 14, and 30-day moving averages
-EMA7 = data.new_cases.ewm(span=7, adjust=False).mean()
-EMA14 = data.new_cases.ewm(span=14, adjust=False).mean()
-EMA30 = data.new_cases.ewm(span=30, adjust=False).mean()
+EMA7 = states.new_cases.ewm(span=7, adjust=False).mean()
+EMA14 = states.new_cases.ewm(span=14, adjust=False).mean()
+EMA30 = states.new_cases.ewm(span=30, adjust=False).mean()
 
-fig = go.Figure()
-fig.add_trace(go.Bar(x=data['date'],y=data['new_cases'],name='Daily Cases'))
-fig.add_trace(go.Scatter(x=data['date'],y=EMA7,mode='lines',name='7-day Moving Average',line=dict(color='yellow')))
-fig.add_trace(go.Scatter(x=data['date'],y=EMA14,name='14-day Moving Average',line=dict(color='turquoise')))
-fig.add_trace(go.Scatter(x=data['date'],y=EMA30,name='30-day Moving Average',line=dict(color='hotpink')))
-fig.update_layout(title='Daily Covid-19 Cases in the US',yaxis_title='Cases',xaxis_title='Date',
+cases = go.Figure()
+cases.add_trace(go.Bar(x=states['date'],y=states['new_cases'],name='Daily Cases'))
+cases.add_trace(go.Scatter(x=states['date'],y=EMA7,mode='lines',name='7-day Moving Average',line=dict(color='yellow')))
+cases.add_trace(go.Scatter(x=states['date'],y=EMA14,name='14-day Moving Average',line=dict(color='turquoise')))
+cases.add_trace(go.Scatter(x=states['date'],y=EMA30,name='30-day Moving Average',line=dict(color='hotpink')))
+cases.update_layout(title='Daily Covid-19 Cases in the US',yaxis_title='Cases',xaxis_title='Date',
                    xaxis={'rangeslider': {'visible':True,}})
 
 #Create histogram of cases
-fig2 = px.histogram(data,x='new_cases',marginal='box')
-fig2.update_layout(title='Daily Case Distribution and Violin Plot',
+box = px.histogram(states,x='new_cases',marginal='box')
+box.update_layout(title='Daily Case Distribution and Violin Plot',
                    yaxis_title='Count',xaxis_title='Daily New Cases')
 
 #Plot total cases over time
-fig3 = px.line(data,x='date',y='cases')
-fig3.update_layout(title='Covid-19 in the US',yaxis_title='Daily Cases',xaxis_title='Date')
+total = px.line(states,x='date',y='cases')
+total.update_layout(title='Covid-19 in the US',yaxis_title='Daily Cases',xaxis_title='Date')
 
-fig4 = go.Figure()
-fig4.add_trace(go.Scatter(x=data['date'],y=data['cases_per_pop'],name='Cases',line=dict(color='yellow'),fill='tonexty'))
-fig4.add_trace(go.Scatter(x=data['date'],y=data['deaths_per_pop'],name='Deaths',line=dict(color='turquoise'),fill='tozeroy'))
-fig4.add_trace(go.Scatter(x=data['date'],y=data['mortality_rate'],name='Mortality Rate',line=dict(color='hotpink'),fill='tonexty'))
-fig4.update_layout(title='Metrics as Percentage of Population',
+pct = go.Figure()
+pct.add_trace(go.Scatter(x=states['date'],y=states['cases_per_pop'],name='Cases',line=dict(color='yellow'),fill='tonexty'))
+pct.add_trace(go.Scatter(x=states['date'],y=states['deaths_per_pop'],name='Deaths',line=dict(color='turquoise'),fill='tozeroy'))
+pct.add_trace(go.Scatter(x=states['date'],y=states['mortality_rate'],name='Mortality Rate',line=dict(color='hotpink'),fill='tonexty'))
+pct.update_layout(title='Metrics as Percentage of Population',
                    yaxis_title='Percent',xaxis_title='Date',
                    xaxis={'rangeslider': {'visible':True,}})
+
+mapfig = px.choropleth(counties_df, geojson=counties, locations='fips', color='cases',
+                           color_continuous_scale="Viridis",
+                           scope="usa",
+                           labels={'cases':'Daily Cases'}
+                          )
+mapfig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},title='COVID-19 Cases by Couny')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -85,47 +105,42 @@ app.layout = html.Div(children=[
 
         dcc.Graph(
             id='EMA',
-            figure=fig
+            figure=cases
         ),  
     ]),
 
     # New Div for all elements in the new 'row' of the page
     html.Div([
-        html.H1(children=''),
-
-        html.Div(children='''
-            
-        '''),
-
         dcc.Graph(
             id='percentages',
-            figure=fig4
+            figure=pct
         ),  
     ]),
 
     html.Div([
-        html.H1(children=''),
-
-        html.Div(children='''
-            
-        '''),
-
         dcc.Graph(
             id='total',
-            figure=fig2
+            figure=total
         ),  
     ]),
 
     html.Div([
-        html.H1(children=''),
-
-        html.Div(children='''
-            
-        '''),
-
         dcc.Graph(
             id='box',
-            figure=fig3
+            figure=box
+        ),  
+    ]),
+     html.Div([
+        dcc.Graph(
+            id='map',
+            figure=mapfig
+        ),  
+    ]),
+
+    html.Div([
+        dcc.Graph(
+            id='box',
+            figure=box
         ),  
     ]),
 ])
